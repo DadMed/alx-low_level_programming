adaf4f89d18224b99422597a9bdcffd5b5b04df9
DadMed <mohammed.dad.ens@gmail.com>
2023-09-10 16:30:47 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,7 @@
-#include <stdio.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Description: print a quote using write fonction
- *	ssize_t  write(int fd,const void *buff,size_t count);
- *
- * Return: 1 (error)
- */
-
-int main(void)
-
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+int main(void) {
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return 1;
 }
+
