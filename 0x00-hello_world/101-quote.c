d7ca72c7f2de676bceeca7b5e3ab883040f1484a
DadMed <mohammed.dad.ens@gmail.com>
2023-09-10 16:13:53 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,14 @@
  * main - Entry point
  *
  * Description writing a quote using write fonction
- * write(int fd,const void *buff,size_t count)
+ *	ssize_t  write(int fd,const void *buff,size_t count);
  *
- * Return 1 (not success)
+ * Return: 1 (error)
  */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(1, quo, 59);
+	write(1, quo, 59);
 	return (1);
 }
