e4c310986c896c7323d2acff3c211852c889449c
DadMed <mohammed.dad.ens@gmail.com>
2023-12-15 15:51:08 -0800

    all tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/DadMed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp/ https://raw.github.com/DadMed/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
